c0e23d183422c38538e740123ffb64477ddbef23
brianweidl <brianweidl@hotmail.com>
2022-05-18 18:05:46 -0300

    add button functionality

--- src/components/RatingContainer.jsx
+++ src/components/RatingContainer.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "../assets/styles/ratingContainer.css";
 import star from "../assets/images/icon-star.svg";
+import thankyouImage from "../assets/images/illustration-thank-you.svg";
 
 function RatingContainer() {
+	const [flip, setFlip] = useState(false);
+	const [rating, setRating] = useState(null);
+	const [selected, setSelected] = useState(false);
+	const warningRef = useRef();
+	const showBack = () => {
+		if (!rating) {
+			warningRef.current.className += " show";
+			return;
+		}
+
+		setFlip(true);
+	};
+
+	const selectRating = (e) => {
+		setRating(e.target.innerText);
+		warningRef.current.className = "warning";
+		setSelected(parseInt(e.target.innerText));
+	};
+
 	return (
-		<div className="container">
+		<div className={`container ${flip ? "flip" : ""}`}>
 			<div className="front">
 				<div className="topStar">
 					<div className="topStar__greyCircle"></div>
@@ -18,13 +38,53 @@ function RatingContainer() {
 					</p>
 				</div>
 				<div className="ratingContainer">
-					<span>1</span>
-					<span>2</span>
-					<span>3</span>
-					<span>4</span>
-					<span>5</span>
+					<span
+						onClick={(e) => selectRating(e)}
+						className={`${selected >= 1 ? "selected" : ""}`}
+					>
+						1
+					</span>
+					<span
+						onClick={(e) => selectRating(e)}
+						className={`${selected >= 2 ? "selected" : ""}`}
+					>
+						2
+					</span>
+					<span
+						onClick={(e) => selectRating(e)}
+						className={`${selected >= 3 ? "selected" : ""}`}
+					>
+						3
+					</span>
+					<span
+						onClick={(e) => selectRating(e)}
+						className={`${selected >= 4 ? "selected" : ""}`}
+					>
+						4
+					</span>
+					<span
+						onClick={(e) => selectRating(e)}
+						className={`${selected === 5 ? "selected" : ""}`}
+					>
+						5
+					</span>
 				</div>
-				<button className="submitButton">SUBMIT</button>
+
+				<button className="submitButton" onClick={showBack}>
+					SUBMIT
+				</button>
+				<div ref={warningRef} className="warning">
+					Choose rating
+				</div>
+			</div>
+			<div className="back">
+				<img className="backImage" src={thankyouImage} alt="" />
+				<div className="ratingSelection">You selected {rating} out of 5</div>
+				<div className="titleText">Thank You!</div>
+				<p>
+					We appreciate you taking the time to give a rating. If you ever need more
+					support, don’t hesitate to get in touch!
+				</p>
 			</div>
 		</div>
 	);
